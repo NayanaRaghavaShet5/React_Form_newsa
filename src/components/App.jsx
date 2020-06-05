9da1cbe5ee29beea0b67e391dd6b629e4edf6c66
NayanaRaghavaShet5 <51402931+NayanaRaghavaShet5@users.noreply.github.com>
2020-06-05 22:15:48 +0530

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 function App() {
   const [isOver, setOver] = useState(false);
+  const [text, setText] = useState("");
+  const [heading, setHeading] = useState("");
 
   function onOver() {
     setOver(true);
@@ -11,17 +13,31 @@ function App() {
     setOver(false);
   }
 
+  function getText(e) {
+    setText(e.target.value);
+  }
+
+  function setData(e) {
+    setHeading(text);
+    //to prevent the default behaviour of the form
+    e.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input on type="text" placeholder="What's your name?" />
-      <button
-        onMouseOver={onOver}
-        onMouseLeave={onLeave}
-        style={{ backgroundColor: isOver ? "black" : "white" }}
-      >
-        Submit
-      </button>
+      <form>
+        <h1>Hello {heading}</h1>
+        <input onChange={getText} type="text" placeholder="What's your name?" />
+        <button
+          type="submit"
+          onMouseOver={onOver}
+          onMouseLeave={onLeave}
+          style={{ backgroundColor: isOver ? "black" : "white" }}
+          onClick={setData}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
